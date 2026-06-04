55fe8e76b4638e04dbcf9d95502b1250415d60a9
Vishwaksen Boga <vishwaksenboga@github.com>
2026-06-04 13:13:36 +0530

    Automate database seeding and admin creation

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Seed the database
+python manage.py seed_products
+
+# Create default admin account if it doesn't exist
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
